e99caf3937c031b361742bcf98e7de5c6d6e52d5
Kev049 <nivek049.gmail>
2018-07-09 14:08:51 +0200

    Ex 12

---
+++ ex12.py
@@ -0,0 +1,12 @@
+a = [5, 10, 15, 20, 25]
+
+
+def firstAndLast(array):
+    newArray = []
+    first = array[0]
+    last = array[len(array) - 1]
+    newArray.append(first)
+    newArray.append(last)
+
+
+firstAndLast(a)
