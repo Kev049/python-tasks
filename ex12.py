1ac18fab950e894e51f7cbbe91ffb76bfeda7be0
Kev049 <nivek049.gmail>
2018-07-10 12:21:33 +0200

    Standard Update (SU)

--- ex12.py
+++ ex12.py
@@ -1,12 +1,18 @@
 a = [5, 10, 15, 20, 25]
 
 
-def firstAndLast(array):
-    newArray = []
-    first = array[0]
-    last = array[len(array) - 1]
-    newArray.append(first)
-    newArray.append(last)
+# def firstAndLast(array):
+#     newArray = []
+#     first = array[0]
+#     last = array[len(array) - 1]
+#     newArray.append(first)
+#     newArray.append(last)
+#     print(newArray)
+#
+#
+# firstAndLast(a)
+def list_ends(a_list):
+    print([a_list[0], a_list[-1]])
 
 
-firstAndLast(a)
+list_ends(a)
